aad5c73b7e1412d53c7050924aa5593e3a337acb
mdrodelo <66499864+mdrodelo@users.noreply.github.com>
2022-10-26 21:08:26 -0400

    Added first templates

--- lw/views.py
+++ lw/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
+
+def index(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
