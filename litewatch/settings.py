4ee0742b9aee2ab595b3de63e635b5ff691ea98f
BrianMcDowell <brianmcdowell@ufl.edu>
2022-11-15 20:56:04 +0000

    adds Procfile for heroku

--- litewatch/settings.py
+++ litewatch/settings.py
@@ -117,12 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
