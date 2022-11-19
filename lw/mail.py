66fd9e3fef8341e30a8bf4cc84b385cda77fac1a
BrianMcDowell <brianmcdowell@ufl.edu>
2022-11-19 19:33:18 +0000

    overwrites mailgun API key

--- lw/mail.py
+++ lw/mail.py
@@ -1,6 +1,6 @@
 import requests
 mailgun_sender = 'sandbox5243c587fa544caa9915e96ddd99a4cd.mailgun.org'
-API_key = '716ca4acc5bc42510a53de7f515f5aab-2de3d545-89158eb8'
+API_key = 'xxxx89158eb8'
 API_base_URL = 'https://api.mailgun.net/v3/sandbox5243c587fa544caa9915e96ddd99a4cd.mailgun.org'
 API_path = API_base_URL + '/messages'
 sender_address = 'sandbox5243c587fa544caa9915e96ddd99a4cd@mailgun.org'
