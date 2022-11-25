c3009a1dcad8e9d57efe8c2342f230f20d67c92c
BrianMcDowell <brianmcdowell@ufl.edu>
2022-11-25 19:45:30 +0000

    adds yml file for secret

--- lw/mail.py
+++ lw/mail.py
@@ -1,6 +1,8 @@
 import requests
+import os
+
 mailgun_sender = 'sandbox5243c587fa544caa9915e96ddd99a4cd.mailgun.org'
-API_key = 'xxxx89158eb8'
+API_key = os.environ['M_API_KEY']
 API_base_URL = 'https://api.mailgun.net/v3/sandbox5243c587fa544caa9915e96ddd99a4cd.mailgun.org'
 API_path = API_base_URL + '/messages'
 sender_address = 'sandbox5243c587fa544caa9915e96ddd99a4cd@mailgun.org'
