09f7d0f82cd8d170f384c42437c51ec1740fd4d1
mdrodelo <66499864+mdrodelo@users.noreply.github.com>
2022-11-30 16:54:39 -0500

    moved db to postgresql and updated search table

--- lw/models.py
+++ lw/models.py
@@ -14,6 +14,8 @@ class Search(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     keyword = models.CharField(max_length=20)
     url = models.CharField(max_length=50)
+    elemtype = models.CharField(max_length=50)
+    elemattr = models.CharField(max_length=100)
     dateCreated = models.DateField(auto_now_add=True)
     state = models.BooleanField(default=True)
 
@@ -23,5 +25,3 @@ class Result(models.Model):
     url = models.CharField(max_length = 255, default="EMPTY URL") # This is the specific url where the keyword was found
     sent = models.BooleanField(default=False) # Can be used to mark whether a result has been sent to the user
     sample = models.TextField(default="")
-
-
